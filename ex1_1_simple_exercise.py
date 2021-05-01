b1ed351a1ad756be37620252d238637bbe160090
nzmars <nzmars@gmail.com>
2021-05-01 23:12:36 +0900

    update

--- ex1_1_simple_exercise.py
+++ ex1_1_simple_exercise.py
@@ -1,14 +1,18 @@
-import keras
-import numpy
+#%%
+from tensorflow import keras
+import numpy as np
 
-x = numpy.array([0, 1, 2, 3, 4])
+x = np.array([0, 1, 2, 3, 4])
+# x = np.arange(10)
+# y = x * 2 + 1 + np.random.randn(x.size)
 y = x * 2 + 1
+n_test = 2
 
 model = keras.models.Sequential()
 model.add(keras.layers.Dense(1, input_shape=(1,)))
 model.compile('SGD', 'mse')
 
-model.fit(x[:2], y[:2], epochs=1000, verbose=0)
+model.fit(x[:n_test], y[:n_test], epochs=1000, verbose=0)
 
 print('Targets:', y[2:])
 print('Predictions:', model.predict(x[2:]).flatten())
